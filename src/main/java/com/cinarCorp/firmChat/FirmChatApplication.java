fcb09ae99d79702325aee03dff9f66ef6c8f1408
ibrahim cinar <cinar-ibrahim@outlook.com>
2024-01-24 15:38:31 +0300

    added packages , OpenAPI and GeneralExceptionHandler Class
    added packages , OpenAPI and GeneralExceptionHandler Class

--- src/main/java/com/cinarCorp/firmChat/FirmChatApplication.java
+++ src/main/java/com/cinarCorp/firmChat/FirmChatApplication.java
@@ -1,7 +1,12 @@
 package com.cinarCorp.firmChat;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FirmChatApplication {
@@ -10,4 +15,12 @@ public class FirmChatApplication {
 		SpringApplication.run(FirmChatApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI springShopOpenAPI(@Value("${application-description}") String description,
+									 @Value("${application-version}") String version) {
+		return new OpenAPI().info(new Info().title("Firm Chat API").version(version)
+				.description(description)
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+	}
 }

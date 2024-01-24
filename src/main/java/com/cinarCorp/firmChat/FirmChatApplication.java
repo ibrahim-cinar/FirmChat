9114a1c6ef5fd2e2452400d1db393d1d94802768
ibrahim cinar <81929391+ibrahim-cinar@users.noreply.github.com>
2024-01-24 15:22:48 +0300

    add files
    add files

---
+++ src/main/java/com/cinarCorp/firmChat/FirmChatApplication.java
@@ -0,0 +1,13 @@
+package com.cinarCorp.firmChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirmChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirmChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinarCorp/firmChat/FirmChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinarCorp.firmChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirmChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
